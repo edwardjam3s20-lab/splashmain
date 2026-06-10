838887033b142b1688f9043f47c5a0bacf361608
Edward <edwardjam3s20@gmail.com>
2026-06-10 14:59:09 +0300

    fix: allow PostHog domains in CSP headers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.posthog.com https://eu-assets.i.posthog.com",
+              "connect-src 'self' https://*.posthog.com https://eu.i.posthog.com https://eu-assets.i.posthog.com https://*.supabase.co",
+              "img-src 'self' data: blob: https:",
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+              "font-src 'self' https://fonts.gstatic.com",
+              "frame-src 'none'",
+            ].join('; '),
+          },
+        ],
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
