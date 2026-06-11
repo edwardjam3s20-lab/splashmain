97e48dbc862c254e08140e367f67acb12b7d15f4
Edward <edwardjam3s20@gmail.com>
2026-06-11 10:06:25 +0300

    fix: remove duplicate PostHog rewrites from next.config, use vercel.json only

--- next.config.js
+++ next.config.js
@@ -1,17 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/ingest/static/:path*',
-        destination: 'https://eu-assets.i.posthog.com/static/:path*',
-      },
-      {
-        source: '/ingest/:path*',
-        destination: 'https://eu.i.posthog.com/:path*',
-      },
-    ]
-  },
   skipTrailingSlashRedirect: true,
 }
 
