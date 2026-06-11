f0d6a2733865b2d1ecd1e1b7adab3d1bfc71063a
Edward <edwardjam3s20@gmail.com>
2026-06-11 18:22:58 +0300

    Add PostHog proxy rewrite and analytics to customer and operator apps

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   skipTrailingSlashRedirect: true,
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://eu-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://eu.i.posthog.com/:path*',
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
